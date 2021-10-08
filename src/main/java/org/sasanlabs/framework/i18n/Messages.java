aa62c8b189b7f16a72f0162f48fc0db98dfaa376
DexLuthor <DexLuthor>
2021-10-08 12:02:48 +0200

    Messages.java added logging

--- src/main/java/org/sasanlabs/framework/i18n/Messages.java
+++ src/main/java/org/sasanlabs/framework/i18n/Messages.java
@@ -1,17 +1,28 @@
 package org.sasanlabs.framework.i18n;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.Locale;
 import java.util.ResourceBundle;
 
-/** @author KSASAN preetkaran20@gmail.com */
+/**
+ * @author KSASAN preetkaran20@gmail.com
+ */
 public class Messages {
+    private static final Logger LOG = LogManager.getLogger(Messages.class);
+
     private static ResourceBundle message =
             ResourceBundle.getBundle(
                     Messages.class.getPackage().getName() + ".messages", Locale.US);
 
     public static String getMessage(String key) {
         if (key != null && message != null && message.containsKey(key)) {
-            return message.getString(key);
+            final String translation = message.getString(key);
+            if (LOG.isTraceEnabled()) {
+                LOG.trace(String.format("%s translated as %s", key, translation));
+            }
+            return translation;
         }
         return "";
     }
