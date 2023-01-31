27a4099f7c886f347e1577803ad94ba80ccbf16d
SampathKumarAmex <sampathkumar.krishnasamykuppusamy@aexp.com>
2023-01-31 19:16:56 +0000

    Interface variables are static and final by default

--- src/main/java/org/sasanlabs/framework/VulnerableAppConstants.java
+++ src/main/java/org/sasanlabs/framework/VulnerableAppConstants.java
@@ -9,8 +9,8 @@ import java.util.List;
  * @author preetkaran20@gmail.com KSASAN
  */
 public interface VulnerableAppConstants {
-    static int DEFAULT_LOAD_ON_STARTUP_VALUE = 10;
-    static String SERVLET_CONTEXT = "VulnerableApp-jsp";
-    static String SLASH = "/";
-    static final List<String> JSP_EXTENSIONS = Arrays.asList("jsp", "jspx");
+    int DEFAULT_LOAD_ON_STARTUP_VALUE = 10;
+    String SERVLET_CONTEXT = "VulnerableApp-jsp";
+    String SLASH = "/";
+    List<String> JSP_EXTENSIONS = Arrays.asList("jsp", "jspx");
 }
